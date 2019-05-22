6526147657912f51a89ac35f6281df5418537cff
kevincrans <kcrans43@gmail.com>
2019-05-22 15:59:27 +0200

    commitment what I'm thinking of

---
+++ Fight for the moon/Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHandler : MonoBehaviour {
+    private int scorePlayer2 = 0;
+    private int scorePlayer1 = 0;
+    public Text scoreplayer1;
+    public Text scoreplayer2;
+    public void addScore(GameObject playerThatDied)
+    {
+        if (playerThatDied.name == "player1")
+        {
+            scorePlayer2++;
+            scoreplayer2.text = scorePlayer2.ToString();
+        }
+        else
+        {
+            scorePlayer1++;
+            scoreplayer1.text = scorePlayer1.ToString();
+        }
+    }
+
+
+
+	
+}
