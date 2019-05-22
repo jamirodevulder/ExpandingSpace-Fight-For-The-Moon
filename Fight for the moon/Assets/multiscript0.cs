5b651b35282dbf8bd33a6b6b577ebfb1ecbdf2f0
kevincrans <kcrans43@gmail.com>
2019-05-22 15:40:05 +0200

    suicideSquad

--- Fight for the moon/Assets/multiscript0.cs
+++ Fight for the moon/Assets/multiscript0.cs
@@ -6,12 +6,14 @@ using UnityEngine.SceneManagement;
 public class multiscript0 : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
     public void StartLevel()
