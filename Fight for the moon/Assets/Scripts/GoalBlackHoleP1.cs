6c68bd7545db2cd1613a001e3d7a4afef388e00c
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2019-06-13 14:37:55 +0200

    Update GoalBlackHoleP1.cs

--- Fight for the moon/Assets/Scripts/GoalBlackHoleP1.cs
+++ Fight for the moon/Assets/Scripts/GoalBlackHoleP1.cs
@@ -11,6 +11,7 @@ public class GoalBlackHoleP1 : MonoBehaviour {
     private GameObject player1;
     private GameObject player2;
     private GameObject ball;
+    private float Transparency = 1f;
     // Use this for initialization
     void Start () {
 		
@@ -37,6 +38,9 @@ public class GoalBlackHoleP1 : MonoBehaviour {
         else if(collision.gameObject.name == "ball")
         {
             ball = collision.gameObject;
+            ball.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            Transparency = 1;
+            StartCoroutine(DelayLoop(collision.gameObject));
         }
 
         GetComponent<AudioSource>().Play();
@@ -51,22 +55,20 @@ public class GoalBlackHoleP1 : MonoBehaviour {
         {
             transform.localScale += new Vector3(0.01F, 0.01F, 0);
         }
-        if (ball != null && ball.transform.localScale.x > 0 && ball.name == "ball")
-        {
-            
-            ball.transform.localScale -= new Vector3(scalespeed, scalespeed, 0);
-            ball.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
-        }
-        if (player1 != null && player1.transform.localScale.x > 0)
-        {
+        //if (ball != null && ball.transform.localScale.x > 0 && ball.name == "ball")
+        //{
+           // ball.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        //}
+      //  if (player1 != null && player1.transform.localScale.x > 0)
+       // {
         //    player1.transform.localScale -= new Vector3(0.1F, 0.1F, 0);
-            player1.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
-        }
-        if (player2 != null && player2.transform.localScale.x > 0)
-        {
+           // player1.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+       // }
+       // if (player2 != null && player2.transform.localScale.x > 0)
+       // {
         //    player2.transform.localScale -= new Vector3(0.1F, 0.1F, 0);
-            player2.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
-        }
+           // player2.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+       // }
     }
 
 
@@ -87,11 +89,12 @@ public class GoalBlackHoleP1 : MonoBehaviour {
             Player.transform.localScale = new Vector3(8, 8, 0);
             Player.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1);
             Player.transform.position = new Vector3(-13, -7.5f, -4.99f);
+            
         }
         else if(Player.name == "ball")
         {
             Player.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
-            Player.transform.localScale = new Vector3(0.5f, 0.5f, 0);
+            Player.transform.localScale = new Vector3(6f, 6f, 0);
             Player.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1);
             Player.transform.position = ballposition;
         }
@@ -113,5 +116,15 @@ public class GoalBlackHoleP1 : MonoBehaviour {
 
 
 
+    }
+    IEnumerator DelayLoop(GameObject ball)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Transparency = Transparency - 0.01f;
+            ball.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, Transparency);
+            Debug.Log(Transparency);
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 }
