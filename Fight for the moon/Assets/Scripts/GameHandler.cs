a4f65a04fd5c652b91c076d0b0f92d773ea7b5ad
kevincrans <kcrans43@gmail.com>
2019-06-17 19:56:35 +0200

    +added DeathFX to lvlScreenBranch, LVLScreen is no rocketbranch

---
+++ Fight for the moon/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHandler : MonoBehaviour {
+    public Image timer;
+    public GameObject player1;
+    public GameObject player2;
+    public Image[] counter;
+    public GameObject countdown;
+    
+    
+	// Use this for initialization
+	void Start () {
+        countdown.SetActive(true);
+        StartCoroutine(startTimer());
+         
+        
+	}
+	
+	// Update is called once per frame
+
+
+    IEnumerator startTimer()
+    {
+        player1.GetComponent<Player1MovementScript>().enabled = false;
+        player2.GetComponent<Player2MovementScript>().enabled = false;
+        timer.GetComponent<Image>().sprite = counter[0].GetComponent<Image>().sprite;
+        yield return new WaitForSeconds(1);
+        timer.GetComponent<Image>().sprite = counter[1].GetComponent<Image>().sprite;
+        yield return new WaitForSeconds(1);
+        timer.GetComponent<Image>().sprite = counter[2].GetComponent<Image>().sprite;
+        yield return new WaitForSeconds(1);
+        timer.GetComponent<Image>().sprite = counter[3].GetComponent<Image>().sprite;
+        yield return new WaitForSeconds(0.5f);
+        countdown.SetActive(false);
+        player1.GetComponent<Player1MovementScript>().enabled = true;
+        player2.GetComponent<Player2MovementScript>().enabled = true;
+    }
+}
---
+++ Fight for the moon/Assets/Scripts/multiscript0.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class multiscript0 : MonoBehaviour {
+    private string[] lvlnames = new string[2];
+    public string lvl;
+     public GameObject pauzescherm;
+    public GameObject tandwiel;
+    public GameObject MainMenu;
+    public GameObject ControllScreenObject;
+    public GameObject CreditsScreenObject;
+    public GameObject backgroundImage;
+    public GameObject LevelScreenObject;
+    public Image ControllScreen;
+    public Image CreditsScreen;
+    public Image MainMenuImage;
+    public Image lvlscreens;
+
+	// Use this for initialization
+	void Start () {
+        lvlnames[0] = "Game";
+        lvlnames[1] = "SoccerGame";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void StartLevel()
+    {
+        MainMenu.SetActive(false);
+        backgroundImage.GetComponent<Image>().sprite = lvlscreens.sprite;
+        LevelScreenObject.SetActive(true);
+    }
+    public void StartSceneGame()
+    {
+        PlayALvl(lvlnames[0]);
+    }
+    public void StartSceneSoccerGame()
+    {
+        PlayALvl(lvlnames[1]);
+        
+    }
+    public void StartSceneCombatScene39420()
+    {
+        SceneManager.LoadScene("CombatScene39420");
+        Debug.Log("+Added: HeroBrine");
+    }
+    public void ToControlls(string lvltoload)
+    {
+        MainMenu.SetActive(false);
+        
+        backgroundImage.GetComponent<Image>().sprite = ControllScreen.sprite;
+        ControllScreenObject.SetActive(true);
+        GameObject.Find("lvlHandler").GetComponent<LvLselector>().setLvl(lvltoload);
+        
+    }
+    public void PlayALvl(string lvltoload)
+    {
+        LevelScreenObject.SetActive(false);
+        ToControlls(lvltoload);
+    }
+    public void StartCredits()
+    {
+        MainMenu.SetActive(false);
+        backgroundImage.GetComponent<Image>().sprite = CreditsScreen.sprite;
+        CreditsScreenObject.SetActive(true);
+    }
+    public void BackMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+    public void Resume()
+    {
+        GameObject.Find("player1").GetComponent<Player1MovementScript>().enabled = true;
+        GameObject.Find("player2").GetComponent<Player2MovementScript>().enabled = true;
+        pauzescherm.SetActive(false);
+        tandwiel.GetComponent<escapePauzeScript>().setEscPressed(false);
+
+    }
+    public void Pauze()
+    {
+        GameObject.Find("player1").GetComponent<Player1MovementScript>().enabled = false;
+        GameObject.Find("player2").GetComponent<Player2MovementScript>().enabled = false;
+        pauzescherm.SetActive(true);
+        tandwiel.GetComponent<escapePauzeScript>().setEscPressed(false);
+    }
+    public void BackFromCredits()
+    {
+        CreditsScreenObject.SetActive(false);
+        backgroundImage.GetComponent<Image>().sprite = MainMenuImage.sprite;
+        MainMenu.SetActive(true);
+    }
+    public void BackFromLvlSelect()
+    {
+        ControllScreenObject.SetActive(false);
+        LevelScreenObject.SetActive(false);
+        backgroundImage.GetComponent<Image>().sprite = MainMenuImage.sprite;
+        MainMenu.SetActive(true);
+    }
+    public void startGame()
+    {
+        
+        
+        SceneManager.LoadScene(GameObject.Find("lvlHandler").GetComponent<LvLselector>().getlvl());
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
