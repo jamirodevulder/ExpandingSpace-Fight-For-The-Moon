2be32065cc5e2e9bd0879b616be503cc43376da2
kevin <kevinb2002@live.nl>
2019-06-25 19:05:00 +0200

    fix to script, final build

--- Fight for the moon/Assets/Scripts/Player2MovementScript.cs
+++ Fight for the moon/Assets/Scripts/Player2MovementScript.cs
@@ -24,6 +24,32 @@ public class Player2MovementScript : MonoBehaviour
     {
         AreaEffector2D area = GetComponent<AreaEffector2D>();
         BoxCollider2D pushCollider = GetComponent<BoxCollider2D>();
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.right * speed);
+            GetComponent<SpriteRenderer>().flipX = true;
+            if (GetComponent<Rigidbody2D>().velocity.magnitude < 2f)
+            {
+                GetComponent<Rigidbody2D>().AddForce(transform.right * firstSpeed);
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.right * -speed);
+            GetComponent<SpriteRenderer>().flipX = false;
+            if (GetComponent<Rigidbody2D>().velocity.magnitude < 2f)
+            {
+                GetComponent<Rigidbody2D>().AddForce(transform.right * -firstSpeed);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            grounded = false;
+            animator.SetBool("Jump", true);
+        }
+
         if (Input.GetKeyDown(KeyCode.DownArrow) && grounded)
         {
             if (rbP2.velocity.x <= 0f)
@@ -75,31 +101,40 @@ public class Player2MovementScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate ()
+    //void FixedUpdate ()
+    //{
+    //    if (Input.GetKey(KeyCode.RightArrow))
+    //    {
+    //        GetComponent<Rigidbody2D>().AddForce(transform.right * speed);
+    //        GetComponent<SpriteRenderer>().flipX = true;
+    //        if(GetComponent<Rigidbody2D>().velocity.magnitude < 2f)
+    //        {
+    //            GetComponent<Rigidbody2D>().AddForce(transform.right * firstSpeed);
+    //        }
+    //    }
+    //    if (Input.GetKey(KeyCode.LeftArrow))
+    //    {
+    //        GetComponent<Rigidbody2D>().AddForce(transform.right * -speed);
+    //        GetComponent<SpriteRenderer>().flipX = false;
+    //        if (GetComponent<Rigidbody2D>().velocity.magnitude < 2f)
+    //        {
+    //            GetComponent<Rigidbody2D>().AddForce(transform.right * -firstSpeed);
+    //        }
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+    //    {
+    //        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    //        grounded = false;
+    //        animator.SetBool("Jump", true);
+    //    }
+    //}
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (collision.gameObject.tag == "Ground")
         {
-            GetComponent<Rigidbody2D>().AddForce(transform.right * speed);
-            GetComponent<SpriteRenderer>().flipX = true;
-            if(GetComponent<Rigidbody2D>().velocity.magnitude < 2f)
-            {
-                GetComponent<Rigidbody2D>().AddForce(transform.right * firstSpeed);
-            }
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            GetComponent<Rigidbody2D>().AddForce(transform.right * -speed);
-            GetComponent<SpriteRenderer>().flipX = false;
-            if (GetComponent<Rigidbody2D>().velocity.magnitude < 2f)
-            {
-                GetComponent<Rigidbody2D>().AddForce(transform.right * -firstSpeed);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
-        {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            grounded = false;
-            animator.SetBool("Jump", true);
+            //Debug.Log("Touch ground!");
+            grounded = true;
+            animator.SetBool("Jump", false);
         }
     }
 
